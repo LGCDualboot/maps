f57560e2116a5cf321eefd3edb99754f68d4deb4
leandrogonzalezcuello <leandro.gonzalez@dualbootpartners.com>
2022-11-04 15:29:52 -0300

    Custom marker with distance

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:dio/dio.dart';
+import 'package:custom_marker/marker_icon.dart';
 import 'package:flutter/material.dart';
-import 'package:golf_map/main.dart';
+import 'package:flutter/services.dart';
+import 'package:golf_map/label_marker_custom.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uuid/uuid.dart';
 
@@ -19,6 +20,8 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   final Completer<GoogleMapController> _controller = Completer();
 
+  final GlobalKey globalKey = GlobalKey();
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -31,47 +34,34 @@ class _MapScreenState extends State<MapScreen> {
   final List<Marker> _markers = <Marker>[];
   final List<Marker> _middlePoints = <Marker>[];
   final List<Marker> _allMarkers = <Marker>[];
+
   final List<Marker> _otherMarkers = <Marker>[];
+
+  final List<LatLng> _polylinePoints = <LatLng>[];
 
   Map<PolylineId, Polyline> _polylines = <PolylineId, Polyline>{};
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.terrain,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-        polylines: Set.of(_polylines.values),
-        markers: Set.of(_allMarkers),
-        onLongPress: (latlng){
-          print("SET OTHER MARKER");
-          _setOtherMarker(latlng);
-        },
-        onTap: (latLng) {
-          print("SET MARKER");
-          _setMarker(latLng);
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          final marker1 = _markers.first;
-          final marker2 = _markers.elementAt(1);
-          _calculateDistanceInMeters(marker1.position, marker2.position);
-          setState(() {
-
-            final newMarker =
-                _addMarkerBetweenTwoPoints(marker1.position, marker2.position);
-            _markers.add(newMarker);
-
-            _calculatePolylineWithMultipleMarkersAndDistance();
-          });
-        },
-        child: Icon(Icons.directions_boat),
-      ),
-    );
+        body: GoogleMap(
+      mapType: MapType.terrain,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+      polylines: Set.of(_polylines.values),
+      markers: Set.of(_allMarkers),
+      onLongPress: (latlng) {
+        print("SET OTHER MARKER");
+        //_setOtherMarker(latlng);
+        _setCustomMarker(latlng);
+      },
+      onTap: (latLng) {
+        print("SET MARKER");
+        _setMarker(latLng);
+      },
+    ));
   }
 
   LatLng _createLatLng(double lat, double lng) {
@@ -120,7 +110,7 @@ class _MapScreenState extends State<MapScreen> {
       consumeTapEvents: true,
       color: Colors.orange,
       width: 2,
-      points: _markers.map((marker) => marker.position).toList(),
+      points: _polylinePoints,
     );
 
     _polylines.clear();
@@ -142,10 +132,11 @@ class _MapScreenState extends State<MapScreen> {
 
       _calculateAllMiddlePoints();
 
+      _setPolylinesPoints(_markers);
+
       _generateAllMarkers();
 
       _calculatePolylineWithMultipleMarkersAndDistance();
-
     });
   }
 
@@ -178,13 +169,10 @@ class _MapScreenState extends State<MapScreen> {
     final y = (firstPoint.longitude + secondPoint.longitude) / 2;
 
     return Marker(
-        markerId: MarkerId(uuid.v4()),
-        position: LatLng(x, y),
-        alpha: 0.5);
+        markerId: MarkerId(uuid.v4()), position: LatLng(x, y), alpha: 0.5);
   }
 
   void _calculateAllMiddlePoints() {
-
     _middlePoints.clear();
 
     for (var i = 0; i < _markers.length - 1; i++) {
@@ -198,7 +186,7 @@ class _MapScreenState extends State<MapScreen> {
     }
   }
 
-  void _setOtherMarker(LatLng latLng){
+  void _setOtherMarker(LatLng latLng) {
     setState(() {
       final Marker marker = Marker(
         markerId: MarkerId(uuid.v4()),
@@ -208,17 +196,34 @@ class _MapScreenState extends State<MapScreen> {
       _otherMarkers.add(marker);
 
       _generateAllMarkers();
-
     });
   }
 
-  void _generateAllMarkers(){
+  void _setPolylinesPoints(List<Marker> markers) {
+    _polylinePoints.clear();
+    _polylinePoints.addAll(markers.map((m) => m.position));
+  }
 
+  Future<void> _generateAllMarkers() async {
     _allMarkers.clear();
 
     _allMarkers.addAll(_markers);
     _allMarkers.addAll(_middlePoints);
     _allMarkers.addAll(_otherMarkers);
+  }
 
+  void _setCustomMarker(LatLng latLng) async {
+    final labelMarker = await customLabelMarker(LabelMarker(
+        label: "EXAMPLE",
+        markerId: MarkerId(uuid.v4()),
+        position: latLng,
+        onTap: () {
+          print("CUSTOM MARKER");
+        }));
+    _otherMarkers.add(labelMarker);
+
+    setState(() {
+      _generateAllMarkers();
+    });
   }
 }
